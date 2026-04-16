f4202def34ae785b3705d2915595c0b6d515e262
nik <n.somma@ied.edu>
2026-04-16 15:03:01 +0200

    falling circle

--- sketch.js
+++ sketch.js
@@ -1,7 +1,28 @@
+let Engine = Matter.Engine,
+    Bodies = Matter.Bodies,
+    Composite = Matter.Composite;
+
+let engine;
+let brush;
+
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  engine = Engine.create();
+
+  brush = Bodies.circle(width/2, height/3, 50);
+
+  Composite.add(engine.world, brush);
 }
 
 function draw() {
   background(220);
+  
+  noStroke();
+  fill(255, 0, 0);
+  circle(brush.position.x, brush.position.y, brush.circleRadius * 2);
+
+  Engine.update(engine);
 }
+  
